0d1cf408fc48a1b090334a06c70d88f6154e1fc2
shun57 <tuktuk.032@gmail.com>
2022-09-13 00:48:01 +0900

    伸び悩んでいる3年目Webエンジニアのための、Python Webアプリケーション自作入門
    Chapter14 「動的に生成したHTML」を返せるようになる

--- python-server/workerthread.py
+++ python-server/workerthread.py
@@ -1,9 +1,12 @@
 import os
+import re
+import textwrap
 import traceback
 from datetime import datetime
+from pprint import pformat
 from socket import socket
 from threading import Thread
-from typing import Tuple
+from typing import Tuple, Optional
 
 class WorkerThread(Thread):
     # 実行ファイルのあるディレクトリ
@@ -43,20 +46,71 @@ class WorkerThread(Thread):
             # HTTPリクエストをパースする
             method, path, http_version, request_header, request_body = self.parse_http_request(request)
 
-            try:
-                # ファイルからレスポンスボディを生成
-                response_body = self.get_static_file_content(path)
+            response_body: bytes
+            content_type: Optional[str]
+            response_line: str
+            # pathが/nowのときは、現在時刻を表示するHTMLを生成する
+            if path == "/now":
+                html = f"""\
+                    <html>
+                    <body>
+                        <h1>Now: {datetime.now()}</h1>
+                    </body>
+                    </html>
+                """
+                response_body = textwrap.dedent(html).encode()
+
+                # Content-Typeを指定
+                content_type = "text/html"
 
                 # レスポンスラインを生成
                 response_line = "HTTP/1.1 200 OK\r\n"
 
-            except OSError:
-                # ファイルが見つからなかった場合は404を返す
-                response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-                response_line = "HTTP/1.1 404 Not Found\r\n"
+            # pathが/show_requestのときは、HTTPリクエストの内容を表示するHTMLを生成する
+            elif path == "/show_request":
+                html = f"""\
+                    <html>
+                    <body>
+                        <h1>Request Line:</h1>
+                        <p>
+                            {method} {path} {http_version}
+                        </p>
+                        <h1>Headers:</h1>
+                        <pre>{pformat(request_header)}</pre>
+                        <h1>Body:</h1>
+                        <pre>{request_body.decode("utf-8", "ignore")}</pre>
+                        
+                    </body>
+                    </html>
+                """
+                response_body = textwrap.dedent(html).encode()
+
+                # Content-Typeを指定
+                content_type = "text/html"
+
+                # レスポンスラインを生成
+                response_line = "HTTP/1.1 200 OK\r\n"
+
+            # pathがそれ以外のときは、静的ファイルからレスポンスを生成する
+            else:
+                try:
+                    response_body = self.get_static_file_content(path)
+
+                    # Content-Typeを指定
+                    content_type = None
+
+                    # レスポンスラインを生成
+                    response_line = "HTTP/1.1 200 OK\r\n"
+                except OSError:
+                    # レスポンスを取得できなかった場合は、ログを出力して404を返す
+                    traceback.print_exc()
+
+                    response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+                    content_type = "text/html"
+                    response_line = "HTTP/1.1 404 Not Found\r\n"
 
             # レスポンスヘッダーを生成
-            response_header = self.build_response_header(path, response_body)
+            response_header = self.build_response_header(path, response_body, content_type)
 
             # レスポンス全体を生成する
             response = (response_line + response_header + "\r\n").encode() + response_body
@@ -75,13 +129,13 @@ class WorkerThread(Thread):
             print(f"=== Worker: クライアントとの通信を終了します remote_address: {self.client_address} ===")
             self.client_socket.close()
 
-    def parse_http_request(self, request: bytes) -> Tuple[str, str, str, bytes, bytes]:
+    def parse_http_request(self, request: bytes) -> Tuple[str, str, str, dict, bytes]:
         """
         HTTPリクエストを
         1. method: str
         2. path: str
         3. http_version: str
-        4. request_header: bytes
+        4. request_header: dict
         5. request_body: bytes
         に分割/変換する
         """
@@ -97,7 +151,13 @@ class WorkerThread(Thread):
         # リクエストラインを文字列に変換してパースする
         method, path, http_version = request_line.decode().split(" ")
 
-        return method, path, http_version, request_header, request_body
+        # リクエストヘッダーを辞書にパースする
+        headers = {}
+        for header_row in request_header.decode().split("\r\n"):
+            key, value = re.split(r": *", header_row, maxsplit=1)
+            headers[key] = value
+
+        return method, path, http_version, headers, request_body
 
     def get_static_file_content(self, path: str) -> bytes:
         """
@@ -106,26 +166,27 @@ class WorkerThread(Thread):
 
         # pathの先頭の/を削除し、相対パスにしておく
         relative_path = path.lstrip("/")
-
         # ファイルのpathを取得
         static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
 
         with open(static_file_path, "rb") as f:
             return f.read()
 
-    def build_response_header(self, path: str, response_body: bytes) -> str:
+    def build_response_header(self, path: str, response_body: bytes, content_type: Optional[str]) -> str:
         """
         レスポンスヘッダーを構築する
         """
-        # ヘッダー生成のためにContent-Typeを取得しておく
-        # pathから拡張子を取得
-        if "." in path:
-            ext = path.rsplit(".", maxsplit=1)[-1]
-        else:
-            ext = ""
-        # 拡張子からMIME Typeを取得
-        # 知らない対応していない拡張子の場合はoctet-streamとする
-        content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+
+        # Content-Typeが指定されていない場合はpathから特定する
+        if content_type is None:
+            # pathから拡張子を取得
+            if "." in path:
+                ext = path.rsplit(".", maxsplit=1)[-1]
+            else:
+                ext = ""
+            # 拡張子からMIME Typeを取得
+            # 知らない対応していない拡張子の場合はoctet-streamとする
+            content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
 
         response_header = ""
         response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
